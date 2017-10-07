552011eef12c382138eba8ca8934d8e120da2ed5
Kacper Woźniak <kacper.wozniak@intive.com>
2017-10-07 23:03:13 +0200

    Initial Commit

---
+++ MVCCVM/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MVCCVM
+//
+//  Created by Kacper Woźniak on 07/10/2017.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MVCCVMTests/MVCCVMTests.swift
@@ -0,0 +1,35 @@
+//
+//  MVCCVMTests.swift
+//  MVCCVMTests
+//
+//  Created by Kacper Woźniak on 07/10/2017.
+//
+
+import XCTest
+@testable import MVCCVM
+
+class MVCCVMTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
